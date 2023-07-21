aae6e881bf3274ddecdb3e1d458a8e5519508b2a
ZubkoA <114995045+ZubkoA@users.noreply.github.com>
2023-07-21 20:01:05 +0300

    start

---
+++ App.js
@@ -0,0 +1,11 @@
+// import { StatusBar } from "expo-status-bar";
+import { View } from "react-native";
+import RegistrationScreen from "./RegistrationScreen/RegistrationScreen";
+
+export default function App() {
+  return (
+    <View>
+      <RegistrationScreen />
+    </View>
+  );
+}
